7767fd104b8ff83976f7523b858ea21b0d1eed72
Saulo Moreira <saulo.moreira@protonmail.com>
2020-12-23 16:25:13 -0500

    Fixed login tabs, save as pdf button and other changes at Transactions window

--- CustomerUI/ViewTransactions.xaml.cs
+++ CustomerUI/ViewTransactions.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using SharedCode;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -119,6 +125,7 @@ namespace CustomerUI
 
             Utils.userTransactions = EFData.context.Transactions.Where(t => t.AccountId ==
             selectedAcc.Id).ToList();
+            
 
             SortTransactionsByTypeAndDate();
             comboHistory.SelectedIndex = 0;
@@ -156,7 +163,64 @@ namespace CustomerUI
 
         private void btPDF_Click(object sender, RoutedEventArgs e)
         {
+            /*using (PdfDocument document = new PdfDocument())
+            {
+                //Add a page to the document
+                PdfPage page = document.Pages.Add();
 
+                //Create PDF graphics for a page
+                PdfGraphics graphics = page.Graphics;
+
+                //Set the standard font
+                PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+                //Draw the text
+                graphics.DrawString("Hello World!!!", font, PdfBrushes.Black, new PointF(0, 0));
+
+                //Save the document
+                document.Save("Output.pdf");
+
+                // TO FIX: Open dialog to choose file name and where to save
+                // TO FIX: Put WPF content in the PDF
+            
+            }*/
+
+            try
+            {
+
+                //Create a new PDF document.
+                PdfDocument doc = new PdfDocument();
+                //Add a page.
+                PdfPage page = doc.Pages.Add();
+                //Create a PdfGrid.
+                PdfGrid pdfGrid = new PdfGrid();
+                //Create a DataTable.
+                DataTable dataTable = new DataTable();
+                //Add columns to the DataTable
+                dataTable.Columns.Add("Transaction Type");
+                dataTable.Columns.Add("Date");
+                dataTable.Columns.Add("Amount");
+                //Add rows to the DataTable.
+                foreach (Transaction t in lvTransactions.Items)
+                {
+                    dataTable.Rows.Add(new object[] { t.Type, t.Date, t.Amount });
+                }
+                //Assign data source.
+                pdfGrid.DataSource = dataTable;
+                //Draw grid to the page of PDF document.
+                pdfGrid.Draw(page, new PointF(10, 10));
+                //Save the document.
+                doc.Save("Output.pdf");
+                //close the document
+                doc.Close(true);
+
+                // TO FIX: Add account holder and balance.
+                // TO FIX: Open dialog to choose where to save file.
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message + "Error");
+            }
         }
     }
 }
